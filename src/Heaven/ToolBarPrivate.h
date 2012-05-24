f0941bfef26d8f997ed9f8e9fa88a278590d292f
Sascha Cunz <Sascha@BabbelBox.org>
2012-05-24 03:01:47 +0200

    Create a 'real' Ui Manager Library.
    Finally managed to get this basicly working. Now on for the rest of the
    implementation bit by bit... :-)

---
+++ src/Heaven/ToolBarPrivate.h
@@ -0,0 +1,38 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_HEAVEN_TOOL_PRIVATE_H
+#define MGV_HEAVEN_TOOL_PRIVATE_H
+
+#include "Heaven/ToolBar.h"
+#include "Heaven/UiContainer.h"
+
+namespace Heaven
+{
+
+	class ToolBarPrivate : public UiContainer
+	{
+	public:
+		ToolBarPrivate( ToolBar* owner );
+		~ToolBarPrivate();
+
+	public:
+		UiObjectTypes type() const;
+	};
+
+}
+
+#endif
---
+++ src/Heaven/UiContainer.h
@@ -0,0 +1,54 @@
+/*
+ * MacGitver
+ * Copyright (C) 2012 Sascha Cunz
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_HEAVEN_UICONTAINER_H
+#define MGV_HEAVEN_UICONTAINER_H
+
+#include <QList>
+
+#include "Heaven/UiObject.h"
+
+namespace Heaven
+{
+
+	class UiContainer : public UiObject
+	{
+		Q_OBJECT
+	protected:
+		UiContainer( QObject* owner );
+		~UiContainer();
+
+	public:
+		void add( UiObject* uio );
+		void remove( UiObject* uio );
+
+	public:
+		bool isContainerDirty() const;
+		virtual void setContainerDirty( bool value = true );
+
+	protected:
+		int numObjects() const;
+		UiObject* objectAt( int index );
+		QList< UiObject* > allObjects() const;
+
+	private:
+		bool					mDirty;
+		QList< UiObject* >		mContent;
+	};
+
+}
+
+#endif
