e64960a7e455830b2e9acc215d688485d74cfd74
Sascha Cunz <Sascha@BabbelBox.org>
2012-11-25 23:51:28 +0100

    Add Dialog class

---
+++ src/libHeaven/App/Dialog.hpp
@@ -0,0 +1,42 @@
+/*
+ * libHeaven - A Qt-based ui framework for strongly modularized applications
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MGV_HEAVEN_DIALOG_H
+#define MGV_HEAVEN_DIALOG_H
+
+#include <QDialog>
+
+#include "libHeaven/HeavenApi.hpp"
+
+namespace Heaven
+{
+
+    /**
+     * @brief A Dialog
+     *
+     * This class automatically gives the Heaven::primaryWindow() as parent to the dialog.
+     *
+     */
+    class HEAVEN_API Dialog : public QDialog
+    {
+        Q_OBJECT
+    public:
+        Dialog();
+    };
+
+}
+
+#endif
