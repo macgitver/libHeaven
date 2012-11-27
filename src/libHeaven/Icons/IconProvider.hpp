9443b06b6063fc5aad20e9b7696751a1d456f8a5
Sascha Cunz <Sascha@BabbelBox.org>
2012-11-27 10:23:28 +0100

    Initial Icon stuff

---
+++ src/libHeaven/Icons/IconProvider.hpp
@@ -0,0 +1,43 @@
+/*
+ * libHeaven - A Qt-based ui framework for strongly modularized applications
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef HAVEN_ICON_PROVIDER_HPP
+#define HAVEN_ICON_PROVIDER_HPP
+
+#include "libHeaven/HeavenApi.hpp"
+
+namespace Heaven
+{
+
+    class Icon;
+    class IconRef;
+
+    class HEAVEN_API IconProvider
+    {
+    protected:
+        IconProvider();
+
+    public:
+        virtual ~IconProvider();
+
+    public:
+        virtual QString name() const = 0;
+        virtual Icon provide( IconRef ref ) = 0;
+    };
+
+}
+
+#endif
---
+++ src/libHeaven/Icons/IconRef.cpp
@@ -0,0 +1,198 @@
+/*
+ * libHeaven - A Qt-based ui framework for strongly modularized applications
+ * Copyright (C) 2012 Sascha Cunz <sascha@babbelbox.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License (Version 2) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QString>
+#include <QStringList>
+
+#include "libHeaven/Icons/IconRef.hpp"
+
+namespace Heaven
+{
+
+    class IconRef::Data : public QSharedData
+    {
+    public:
+        IconProvider*       provider;
+        QString             text;
+        int                 size;
+        QStringList         parameters;
+        IconRef             refParam;
+    };
+
+    IconRef::IconRef()
+    {
+    }
+
+    IconRef::IconRef( const IconRef& other )
+    {
+        d = other.d;
+    }
+
+    IconRef::IconRef( IconProvider* provider, const char* pszText, int size )
+    {
+        set( provider, QString::fromUtf8( pszText ), size );
+    }
+
+    IconRef::IconRef( IconProvider* provider, const QString& text, int size )
+    {
+        set( provider, text, size );
+    }
+
+    IconRef::IconRef( const QString& text, int size )
+    {
+        set( NULL, text, size );
+    }
+
+    IconRef::IconRef( const char* pszText, int size )
+    {
+        set( NULL, QString::fromUtf8( pszText ), size );
+    }
+
+    IconRef::~IconRef()
+    {
+    }
+
+    IconRef& IconRef::operator=( const IconRef& other )
+    {
+        d = other.d;
+        return * this;
+    }
+
+    IconRef& IconRef::operator=( const QString& text )
+    {
+        return *this = IconRef( text, -1 );
+    }
+
+    IconRef& IconRef::operator=( const char* pszText )
+    {
+        return *this = IconRef( pszText, -1 );
+    }
+
+    bool IconRef::operator==( const IconRef& other ) const
+    {
+        if( d.constData() == other.d.constData() )
+            return true;
+
+        if( !d || !other.d )
+            return false;
+
+        return d->provider == other.d->provider &&
+                d->text == other.d->text &&
+                d->size == other.d->size &&
+                d->parameters == other.d->parameters &&
+                d->refParam == other.d->refParam;
+    }
+
+    bool IconRef::isValid() const
+    {
+        return d.data() != NULL;
+    }
+
+    QString IconRef::toString() const
+    {
+        return QString();
+    }
+
+    IconRef IconRef::fromString( const QString& str )
+    {
+        return IconRef();
+    }
+
+    IconRef IconRef::fromString( const char* pszStr )
+    {
+        return fromString( QString::fromUtf8( pszStr ) );
+    }
+
+    IconProvider* IconRef::provider() const
+    {
+        Q_ASSERT( d );
+        return d->provider;
+    }
+
+    QString IconRef::text() const
+    {
+        Q_ASSERT( d );
+        return d->text;
+    }
+
+    int IconRef::size() const
+    {
+        Q_ASSERT( d );
+        return d->size;
+    }
+
+    int IconRef::numParameters() const
+    {
+        Q_ASSERT( d );
+        return d->parameters.count() + ( d->refParam.isValid() ? 1 : 0 );
+    }
+
+    QString IconRef::parameter( int index ) const
+    {
+        Q_ASSERT( d );
+        if( index == d->parameters.count() )
+        {
+            return d->refParam.toString();
+        }
+
+        return d->parameters.at( index );
+    }
+
+    void IconRef::setSize( int size )
+    {
+        Q_ASSERT( d );
+        d->size = size;
+    }
+
+    void IconRef::setText( const QString& text )
+    {
+        Q_ASSERT( d );
+        d->text = text;
+    }
+
+    void IconRef::setProvider( IconProvider* provider )
+    {
+        Q_ASSERT( d );
+        d->provider = provider;
+    }
+
+    void IconRef::appendParam( const QString& text )
+    {
+        Q_ASSERT( d );
+        d->parameters.append( text );
+    }
+
+    void IconRef::appendParam( const char* szText )
+    {
+        appendParam( QString::fromUtf8( szText ) );
+    }
+
+    void IconRef::appendParam( const IconRef& other )
+    {
+        Q_ASSERT( d );
+        Q_ASSERT( !d->refParam.isValid() );
+        d->refParam = other;
+    }
+
+    void IconRef::set( IconProvider* provider, const QString& text, int size )
+    {
+        d = new Data;
+        d->provider = provider;
+        d->text = text;
+        d->size = size;
+    }
+
+}
